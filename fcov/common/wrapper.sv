bf150d863f33f6bf1993411f773b210cc6310327
Huda-10xe <huda.sajjad@10xengineers.ai>
2024-08-23 11:33:20 +0500

    Changing VM cases to be covered by testcase to assertions

--- fcov/common/wrapper.sv
+++ fcov/common/wrapper.sv
@@ -2,6 +2,4 @@ module wrapper import cvw::*; #(parameter cvw_t P) (input clk);
     RV64VM_coverage #(P) vm_inst(clk);
     RV64VM_PMP_coverage #(P) vm_pmp_inst(clk);
     RV64Zicbom_coverage #(P) zicbom_inst(clk);
-    RV64Zicntr_coverage #(P) zicntr_inst(clk);
-    RV64Zihpm_coverage #(P) zihpm_inst(clk);
 endmodule
