62ced43b96f51610a2aeb6113e6157cd5a19762f
David Harris <David_Harris@hmc.edu>
2024-09-22 20:49:20 -0700

    Starting to add cr format compressed instructions, but no coverage yet

--- bin/testgen.py
+++ bin/testgen.py
@@ -55,9 +55,12 @@ def writeCovVector(desc, rs1, rs2, rd, rs1val, rs2val, immval, rdval, test, xlen
     lines = lines + test + " x" + str(rd) + ", x" + str(rs1) + ", x" + str(rs2) + " # perform operation\n" 
   elif (test in citype):
     if(test == "c.lui" and rd ==2): # rd ==2 is illegal operand 
-        rd = 9 # change to arbitrary other register
+      rd = 9 # change to arbitrary other register
     lines = lines + test + " x" + str(rd) + ", " + unsignedImm6(immval) + " # perform operation\n"
   elif (test in crtype):
+    if (test == "c.add" and (rd == 0 or rs2 == 0)):
+      rd = 10
+      rs2 = 11
     lines = lines + test + " x" + str(rd) + ", x" + str(rs2) + " # perform operation\n"
   elif (test in shiftitype):
     lines = lines + "li x" + str(rs1) + ", " + formatstr.format(rs1val) + " # initialize rs1\n"
