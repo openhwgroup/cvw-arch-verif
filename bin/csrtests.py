a908523b7b80e501767ea9736b2aba50d731aae0
David Harris <David_Harris@hmc.edu>
2024-10-26 03:10:26 -0700

    Improve CSR test generation

--- bin/csrtests.py
+++ bin/csrtests.py
@@ -48,52 +48,61 @@ def csrwalk(pathname, regs):
         print("\tcsrrw t6, "+reg+", s0    # restore CSR")
     outfile.close
 
+def csrtests(pathname, US):
+    outfile = open(pathname, 'w')
+    sys.stdout = outfile
+    if (US): # User/Super mode
+        rng = range(0x3A0, 0x3F0)
+        print("// Additional tests to (unsuccessfully) write PMP from S and U mode; running these in M mode mucks up the PMP and hangs the DUT")
+    else:
+        rng = range(4096)
+    for i in rng:
+        # 7a0-7a5 are debug/trace registers implemented in ImperasDV but not Wally (Issue 1031) ***skipped until fixed
+        while (i == mseccfgReg or (i >= 0x7a0 and i <= 0x7a5)): # *** skip msseccfg tests causing mismatches for now *** FIX ME *** Issue 1007
+            i = i + 1
+        reg1 = randint(1, 31)
+        reg2 = randint(1, 31)
+        reg3 = randint(1, 31)
+        while (reg2 == reg1):
+            reg2 = randint(1, 31)
+        while (reg3 == reg1 or reg3 == reg2):
+            reg3 = randint(1, 31)
+        reg1 = str(reg1)
+        reg2 = str(reg2)
+        reg3 = str(reg3)
+        ih = hex(i)
+        print("\n// Testing CSR "+ih)
+        print("\tcsrr x"+str(reg1)+", "+ ih + "\t// Read CSR")   
+        if (i < 0x3A0 or i > 0x3EF or US): # skip writing PMP registers, which are touchy and tested separately
+            print("\tli x"+reg2+", -1")
+            print("\tcsrrw x"+reg3+", "+ih+", x"+reg2+"\t// Write all 1s to CSR") 
+            print("\tcsrrw x"+reg3+", "+ih+", x0\t// Write all 0s to CSR")
+            print("\tcsrrs x"+reg3+", "+ih+", x"+reg2+"\t// Set all CSR bits")
+            print("\tcsrrc x"+reg3+", "+ih+", x"+reg2+"\t// Clear all CSR bits")
+            print("\tcsrrw x"+reg3+", "+ih+", x"+reg1+"\t// Restore CSR")   
+        else:
+            print("\t// Don't try reading or writing PMP in machine mode because the weird patterns hang the DUT")
+    outfile.close
+
 # setup
 seed(0) # make tests reproducible
 
 # generate repetitive assembly language tests
 
 # writable registers to test with walking 1s and 0s
-# *** "minstret" commented out of mregs for now per issue #210
 mregs = ["mstatus", "mcause", "misa", "medeleg", "mideleg", "mie", "mtvec", "mcounteren", "mscratch", "mepc", "mtval", "mip", "menvcfg", "mcycle", "mcountinhibit", "mstatush", "mcycleh", "minstreth"] # ***removed "mseccfg" until Issue 1007 resolved 
 sregs = ["sstatus", "scause", "sie", "stvec", "scounteren", "senvcfg", "sscratch", "sepc", "stval", "sip", "satp", "0x120", "stimecmp", "stimecmph"]
 uregs = ["fflags", "frm", "fcsr"]
 
-seedReg = 0x015
 mseccfgReg = 0x747
 
 WALLY = os.environ.get('WALLY')
-pathname = WALLY+"/addins/cvw-arch-verif/tests/priv/Zicsr-CSR-Tests.h"
-outfile = open(pathname, 'w')
-sys.stdout = outfile
 
-# test pmp separately (3A0-3EF)
-# 323-33F are mhpmevent; should be tied to 0 (i >= 0x323 and i <= 0x33F) or 
-# 7a0-7a5 are debug registers 
-for i in range(4096):
-    while (i == mseccfgReg or (i >= 0x7a0 and i <= 0x7a5)): # *** skip msseccfg tests causing mismatches for now *** FIX ME *** Issue 1007
-        i = i + 1
-    reg1 = randint(1, 31)
-    reg2 = randint(1, 31)
-    reg3 = randint(1, 31)
-    while (reg2 == reg1):
-        reg2 = randint(1, 31)
-    while (reg3 == reg1 or reg3 == reg2):
-        reg3 = randint(1, 31)
-    reg1 = str(reg1)
-    reg2 = str(reg2)
-    reg3 = str(reg3)
-    ih = hex(i)
-    print("\n// Testing CSR "+ih)
-    print("\tcsrr x"+str(reg1)+", "+ ih + "\t// Read CSR")   
-    if (i < 0x3A0 or i > 0x3EF): # skip writing PMP registers, which are touchy and tested separately
-        print("\tli x"+reg2+", -1")
-        print("\tcsrrw x"+reg3+", "+ih+", x"+reg2+"\t// Write all 1s to CSR") 
-        print("\tcsrrw x"+reg3+", "+ih+", x0\t// Write all 0s to CSR")
-        print("\tcsrrs x"+reg3+", "+ih+", x"+reg2+"\t// Set all CSR bits")
-        print("\tcsrrc x"+reg3+", "+ih+", x"+reg2+"\t// Clear all CSR bits")
-        print("\tcsrrw x"+reg3+", "+ih+", x"+reg1+"\t// Restore CSR")   
-outfile.close
+pathname = WALLY+"/addins/cvw-arch-verif/tests/priv/Zicsr-CSR-Tests.h"
+csrtests(pathname, False) # make tests for M mode
+
+pathname = WALLY+"/addins/cvw-arch-verif/tests/priv/Zicsr-CSR-USPMPWrite-Tests.h"
+csrtests(pathname, True) # additional tests to read/write PMP registers in U/S mode
 
 pathname = WALLY+"/addins/cvw-arch-verif/tests/priv/ZicsrM-Walk.h"
 csrwalk(pathname, mregs + sregs + uregs);
