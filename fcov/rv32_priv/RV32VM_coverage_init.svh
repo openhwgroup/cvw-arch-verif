eaf3754d125e5b41685199e2e4fd2c0f97117181
David Harris <David_Harris@hmc.edu>
2024-11-08 08:22:45 -0800

    fixed name of obj_mstatus_mprv covergroup

--- fcov/rv32_priv/RV32VM_coverage_init.svh
+++ fcov/rv32_priv/RV32VM_coverage_init.svh
@@ -11,7 +11,7 @@
     satp_cg = new(); satp_cg.set_inst_name("obj_satp");
     PA_VA_cg = new(); PA_VA_cg.set_inst_name("obj_PA_VA");
     sfence_cg = new(); sfence_cg.set_inst_name("obj_sfence");
-    mstatus_mprv_cg = new(); mstatus_cg.set_inst_name("obj_mstatus"); 
+    mstatus_mprv_cg = new(); mstatus_mprv_cg.set_inst_name("obj_mstatus_mprv"); 
     vm_permissions_cg = new(); vm_permissions_cg.set_inst_name("obj_vm_permissions");
     res_global_pte_cg = new(); res_global_pte_cg.set_inst_name("obj_res_global_pte");
     add_feature_cg = new(); add_feature_cg.set_inst_name("obj_add_feature");
