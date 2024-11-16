ba75cd5838486ea546f921eced60c25ceb6323e2
Corey Hickson <chickson@hmc.edu>
2024-11-15 22:26:55 -0800

    refactored ZicsrU

--- fcov/priv/ZicsrU_coverage.svh
+++ fcov/priv/ZicsrU_coverage.svh
@@ -24,8 +24,8 @@
 typedef RISCV_instruction #(ILEN, XLEN, FLEN, VLEN, NHART, RETIRE) ins_zicsru_t;
 
 covergroup ucsr_cg with function sample(ins_zicsru_t ins);
-    option.per_instance = 1; 
-    option.comment = "ZicsrU ucsr";
+    option.per_instance = 0; 
+    // "ZicsrU ucsr"
 
     // building blocks for the main coverpoints
 
@@ -38,12 +38,6 @@ covergroup ucsr_cg with function sample(ins_zicsru_t ins);
     }
     csrrw: coverpoint ins.current.insn {
         wildcard bins csrrw = {32'b????????????_?????_001_?????_1110011}; 
-    }
-    csrrs: coverpoint ins.current.insn {
-        wildcard bins csrrs = {32'b????????????_?????_010_?????_1110011};
-    }
-    csrrc: coverpoint ins.current.insn {
-        wildcard bins csrrc = {32'b????????????_?????_011_?????_1110011};
     }
     csr: coverpoint ins.current.insn[31:20]  {
     // automtically gives all 4096 bins
@@ -70,13 +64,10 @@ covergroup ucsr_cg with function sample(ins_zicsru_t ins);
 endgroup
 
 covergroup mstatus_u_cg with function sample(ins_zicsru_t ins);
-    option.per_instance = 1; 
-    option.comment = "ZicsrU mstatus UBE";
+    option.per_instance = 0; 
+    // "ZicsrU mstatus UBE"
 
     // building blocks for the main coverpoints
-    cp_sd: coverpoint ins.current.insn {
-        wildcard bins sd = {32'b????????????_?????_011_?????_0100011}; 
-    }
     cp_sw: coverpoint ins.current.insn {
         wildcard bins sw = {32'b????????????_?????_010_?????_0100011}; 
     }
@@ -86,14 +77,8 @@ covergroup mstatus_u_cg with function sample(ins_zicsru_t ins);
     cp_sb: coverpoint ins.current.insn {
         wildcard bins sb = {32'b????????????_?????_000_?????_0100011}; 
     }
-    cp_ld: coverpoint ins.current.insn {
-        wildcard bins ld = {32'b????????????_?????_001_?????_0000011}; 
-    }
     cp_lw: coverpoint ins.current.insn {
         wildcard bins lw = {32'b????????????_?????_010_?????_0000011}; 
-    }
-    cp_lwu: coverpoint ins.current.insn {
-        wildcard bins lwu = {32'b????????????_?????_110_?????_0000011}; 
     }
     cp_lh: coverpoint ins.current.insn {
         wildcard bins lh = {32'b????????????_?????_001_?????_0000011}; 
@@ -124,14 +109,14 @@ covergroup mstatus_u_cg with function sample(ins_zicsru_t ins);
     mstatus_mprv: coverpoint ins.current.csr[12'h300][17] { // mprv is mstatus[17]
     }
     mstatus_mpp: coverpoint ins.current.csr[12'h300][12:11] { // mprv is mstatus[12:11]
-        bins zeros = {'0};
-        bins ones  = {'1};
+        bins U_Mode = {2'b00};
+        bins M_Mode = {2'b11};
     }
     `ifdef XLEN64
-    mstatus_mbe: coverpoint ins.current.csr[12'h300][37] { // mbe is mstatus[37] in RV64
+        mstatus_mbe: coverpoint ins.current.csr[12'h300][37] { // mbe is mstatus[37] in RV64
     }
     `else
-    mstatus_mbe: coverpoint ins.current.csr[12'h310][5] { // mbe is mstatush[5] in RV32
+        mstatus_mbe: coverpoint ins.current.csr[12'h310][5] { // mbe is mstatush[5] in RV32
     }
     `endif
     
@@ -156,21 +141,31 @@ covergroup mstatus_u_cg with function sample(ins_zicsru_t ins);
     cp_mstatus_mprv_ube_endianness_lbu: cross priv_mode_u, mstatus_ube, cp_lbu, cp_byteoffset, mstatus_mprv, mstatus_mbe, mstatus_mpp;
 
     `ifdef XLEN64
-    cp_doubleoffset: coverpoint ins.current.imm[2:0] iff (ins.current.rs1_val[2:0] == 3'b000)  {
-        bins zero = {3'b000};
-    }
-    cp_mstatus_ube_endianness_sd:  cross priv_mode_u, mstatus_ube, cp_sd;
-    cp_mstatus_ube_endianness_ld:  cross priv_mode_u, mstatus_ube, cp_ld;
-    cp_mstatus_ube_endianness_lwu: cross priv_mode_u, mstatus_ube, cp_lwu, cp_wordoffset;
-    cp_mstatus_mpr_ube_endianness_sd:  cross priv_mode_u, mstatus_ube, cp_sd, mstatus_mprv, mstatus_mbe, mstatus_mpp;
-    cp_mstatus_mpr_ube_endianness_ld:  cross priv_mode_u, mstatus_ube, cp_ld, mstatus_mprv, mstatus_mbe, mstatus_mpp;
-    cp_mstatus_mpr_ube_endianness_lwu: cross priv_mode_u, mstatus_ube, cp_lwu, cp_wordoffset, mstatus_mprv, mstatus_mbe, mstatus_mpp;
+
+        cp_sd: coverpoint ins.current.insn {
+            wildcard bins sd = {32'b????????????_?????_011_?????_0100011}; 
+        }
+        cp_ld: coverpoint ins.current.insn {
+            wildcard bins ld = {32'b????????????_?????_001_?????_0000011}; 
+        }
+        cp_lwu: coverpoint ins.current.insn {
+            wildcard bins lwu = {32'b????????????_?????_110_?????_0000011}; 
+        }
+        cp_doubleoffset: coverpoint ins.current.imm[2:0] iff (ins.current.rs1_val[2:0] == 3'b000)  {
+            bins zero = {3'b000};
+        }
+        cp_mstatus_ube_endianness_sd:  cross priv_mode_u, mstatus_ube, cp_sd, cp_doubleoffset;
+        cp_mstatus_ube_endianness_ld:  cross priv_mode_u, mstatus_ube, cp_ld, cp_doubleoffset;
+        cp_mstatus_ube_endianness_lwu: cross priv_mode_u, mstatus_ube, cp_lwu, cp_wordoffset;
+        cp_mstatus_mpr_ube_endianness_sd:  cross priv_mode_u, mstatus_ube, cp_sd, mstatus_mprv, mstatus_mbe, mstatus_mpp;
+        cp_mstatus_mpr_ube_endianness_ld:  cross priv_mode_u, mstatus_ube, cp_ld, mstatus_mprv, mstatus_mbe, mstatus_mpp;
+        cp_mstatus_mpr_ube_endianness_lwu: cross priv_mode_u, mstatus_ube, cp_lwu, cp_wordoffset, mstatus_mprv, mstatus_mbe, mstatus_mpp;
     `endif
 endgroup
 
 covergroup uprivinst_cg with function sample(ins_zicsru_t ins);
-    option.per_instance = 1; 
-    option.comment = "ZicsrU uprivinst";
+    option.per_instance = 0; 
+    // "ZicsrU uprivinst"
 
     // building blocks for the main coverpoints
     instrs: coverpoint ins.current.insn {
