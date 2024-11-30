ef58cd09e2d44bd2c06891f2c695f3700fa9152c
David Harris <David_Harris@hmc.edu>
2024-11-29 16:11:59 -0800

    Code cleanup

--- bin/illegalinstrtests.py
+++ bin/illegalinstrtests.py
@@ -78,9 +78,8 @@ gen("cp_fence_cbo", "RRRRRRRRRRRRRRRRREEERRRRR0001111")
 gen("cp_cbo_immediate", "EEEEEEEEEEEE00000010000000001111") # tie rs1 = 0 to avoid overwriting program on cbo.zero
 gen("cp_cbo_rd",        "00000000000RRRRRR010EEEEE0001111")
 gen("cp_Itype",         "EEEEEEEEEEEERRRRRE01RRRRR0010011", 32, 
-                    ["000010001111XXXXXX01XXXXX0010011", # Exclude zip and unzip until ImperasDV is fixed per issue #1164 
-                     "00010000100XXXXXX001XXXXX0010011" # Exclude sm3p0 and sm3p1 until disabled in imperas.ic
-                     ])
+                    ["00010000100XXXXXX001XXXXX0010011" ]) # Exclude sm3p0 and sm3p1 until disabled in imperas.ic
+                    
 gen("cp_IWtype",        "RRRRRRRRRRRRRRRRREEERRRRR0011011")
 gen("cp_IWshift",       "EEEEEEERRRRRRRRRRE01RRRRR0011011")
 gen("cp_store",         "RRRRRRRRRRRR00000EEERRRRR0100011") # use rs1 = 0 for stores to avoid overwriting program
@@ -89,10 +88,7 @@ gen("cp_atomic_funct3", "RRRRRRRRRRRR00000EEERRRRR0101111") # use rs1 = 0 for at
 gen("cp_atomic_funct7", "EEEEERRRRRRR0000001ERRRRR0101111") # use rs1 = 0 for atomics to avoid overwriting program
 gen("cp_lrsc",          "00010RREEEEE0000001ERRRRR0101111")
 gen("cp_rtype",         "EEEEEEERRRRRRRRRREEERRRRR0110011", 32,
-                        ["0000100XXXXXXXXXX100XXXXX0110011", # Exclude pack until ImperasDV is fixed per issue #1148
-                         "0000100XXXXXXXXXX111XXXXX0110011", # also packh per 1148
-                         "XX110X0XXXXXXXXXX000XXXXX0110011" # Exclude sm4ks and sm4ed until disabled in imperas.ic
-    ]) 
+                        ["XX110X0XXXXXXXXXX000XXXXX0110011"]) # Exclude sm4ks and sm4ed until disabled in imperas.ic
 gen("cp_rwtype",        "EEEEEEERRRRRRRRRREEERRRRR0111011")
 gen("cp_ftype",         "EEEEERRRRRRRRRRRREEERRRRR1010011")
 gen("cp_fsqrt",         "0101100EEEEERRRRRRRRRRRRR1010011") 
