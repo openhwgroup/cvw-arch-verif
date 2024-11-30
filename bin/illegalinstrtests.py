d385c41bfd73a6d530b6901d10ada55e73f6c1d2
David Harris <David_Harris@hmc.edu>
2024-11-30 15:45:42 -0800

    Modified AMO instructino tests to avoid warning messages about reading x from memory

--- bin/illegalinstrtests.py
+++ bin/illegalinstrtests.py
@@ -85,7 +85,18 @@ gen("cp_IWshift",       "EEEEEEERRRRRRRRRRE01RRRRR0011011")
 gen("cp_store",         "RRRRRRRRRRRR00000EEERRRRR0100011") # use rs1 = 0 for stores to avoid overwriting program
 gen("cp_fstore",        "RRRRRRRRRRRR00000EEERRRRR0100111") # use rs1 = 0 for stores to avoid overwriting program
 gen("cp_atomic_funct3", "RRRRRRRRRRRR00000EEERRRRR0101111") # use rs1 = 0 for atomics to avoid overwriting program
-gen("cp_atomic_funct7", "EEEEERRRRRRR0000001ERRRRR0101111") # use rs1 = 0 for atomics to avoid overwriting program
+gen("cp_atomic_funct7", "EEEEERRRRRRR0000001ERRRRR0101111", 32, # use rs1 = 0 for atomics to avoid overwriting program
+                        [
+                            "00001XXXXXXXXXXXX01XXXXXX0101111", # exclude amoswap to avoid stores to radom locations
+                            "00000XXXXXXXXXXXX01XXXXXX0101111", # exclude amoadd to avoid stores to radom locations
+                            "00100XXXXXXXXXXXX01XXXXXX0101111", # exclude amoxor to avoid stores to radom locations
+                            "01100XXXXXXXXXXXX01XXXXXX0101111", # exclude amoand to avoid stores to radom locations
+                            "01000XXXXXXXXXXXX01XXXXXX0101111", # exclude amoor to avoid stores to radom locations
+                            "10000XXXXXXXXXXXX01XXXXXX0101111", # exclude amomin to avoid stores to radom locations
+                            "10100XXXXXXXXXXXX01XXXXXX0101111", # exclude amomax to avoid stores to radom locations
+                            "11000XXXXXXXXXXXX01XXXXXX0101111", # exclude amominu to avoid stores to radom locations
+                            "11100XXXXXXXXXXXX01XXXXXX0101111"  # exclude amomaxu to avoid stores to radom locations
+                        ])
 gen("cp_lrsc",          "00010RREEEEE0000001ERRRRR0101111")
 gen("cp_rtype",         "EEEEEEERRRRRRRRRREEERRRRR0110011", 32,
                         ["XX110X0XXXXXXXXXX000XXXXX0110011"]) # Exclude sm4ks and sm4ed until disabled in imperas.ic
