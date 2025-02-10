dbd1a65d17e170ba7a1abfc68b412143f086000e
Jordan Carlin <jordanmcarlin@gmail.com>
2025-02-10 00:55:57 -0800

    Rename main coverage module

--- fcov/cvw-arch-verif.f
+++ fcov/cvw-arch-verif.f
@@ -1,7 +1,11 @@
+// Include all coverpoints
 +incdir+${CVW_ARCH_VERIF}/fcov/unpriv
 +incdir+${CVW_ARCH_VERIF}/fcov/priv
 +incdir+${CVW_ARCH_VERIF}/fcov/rv64_priv
 +incdir+${CVW_ARCH_VERIF}/fcov/rv32_priv
+// Include coverage classes and helper functions
 +incdir+${CVW_ARCH_VERIF}/fcov
+// Trace module
 ${CVW_ARCH_VERIF}/fcov/rvviTrace.sv
-${CVW_ARCH_VERIF}/fcov/trace2riscvISACOV.sv
+// Main coverage module and class instantiation
+${CVW_ARCH_VERIF}/fcov/cvw_arch_verif.sv
--- fcov/cvw_arch_verif.sv
+++ fcov/cvw_arch_verif.sv
@@ -1,30 +1,32 @@
-// trace2riscvISACOV.sv
+// cvw_arch_verif.sv
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 
 // Load which extensions are supported in this configuration (from $WALLY/config/<config>/coverage.svh)
 `include "coverage.svh"
+
+// Load disassembler
 `include "disassemble.svh"
 
 // Load the coverage classes
 `include "RISCV_coverage.svh"
 
-module trace2riscvISACOV(rvviTrace rvvi);
+module cvw_arch_verif(rvviTrace rvvi);
   string disass;
   // Connect coverage class to RVVI trace interface
   coverage #(rvvi.ILEN, rvvi.XLEN, rvvi.FLEN, rvvi.VLEN, rvvi.NHART, rvvi.RETIRE) riscvISACOV;
   initial begin
     riscvISACOV = new(rvvi);
-    $display("trace2riscvISACOV: coverage initialized");
+    $display("cvw_arch_verif: coverage initialized");
   end
 
-  // Invoke the riscvISACOV sample function on each clock edge for the current Instruction
+  // Invoke the riscvISACOV sample function on each clock edge for the current instruction
   // If RVVI accepts more than one instruction or hart, iterate over all of them in the
   // correct order of retirement (TODO: multiple instructions/harts not implemented)
   always_ff @(posedge rvvi.clk) begin
     if (rvvi.valid[0][0] == 1) begin
       disass = disassemble(rvvi.insn[0][0]);
       riscvISACOV.sample(rvvi.trap[0][0], 0, 0, {$sformatf("%h ", rvvi.insn[0][0]), disass});
-      // $display("trace2riscvISACOV: sample taken for instruction %h: %s", rvvi.insn[0][0], disass);
+      // $display("cvw_arch_verif: sample taken for instruction %h: %s", rvvi.insn[0][0], disass);
       $display("0x%h: %s", rvvi.insn[0][0], disass);
     end
   end
