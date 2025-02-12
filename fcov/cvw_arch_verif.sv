2a46f6cc475d332db88907b5cda0b1740379c172
Jordan Carlin <jordanmcarlin@gmail.com>
2025-02-12 04:47:35 -0800

    For compressed instructions only use bottom 16 bits

--- fcov/cvw_arch_verif.sv
+++ fcov/cvw_arch_verif.sv
@@ -12,6 +12,9 @@
 
 module cvw_arch_verif(rvviTrace rvvi);
   string disass;
+  logic compressedInstruction;
+  logic [31:0] trimmedInsn;
+
   // Connect coverage class to RVVI trace interface
   coverage #(rvvi.ILEN, rvvi.XLEN, rvvi.FLEN, rvvi.VLEN, rvvi.NHART, rvvi.RETIRE) riscvISACOV;
   initial begin
@@ -24,10 +27,21 @@ module cvw_arch_verif(rvviTrace rvvi);
   // correct order of retirement (TODO: multiple instructions/harts not implemented)
   always_ff @(posedge rvvi.clk) begin
     if (rvvi.valid[0][0] == 1) begin
-      disass = disassemble(rvvi.insn[0][0]);
-      riscvISACOV.sample(rvvi.trap[0][0], 0, 0, {$sformatf("%h ", rvvi.insn[0][0]), disass});
-      // $display("cvw_arch_verif: sample taken for instruction %h: %s", rvvi.insn[0][0], disass);
-      $display("0x%h: %s", rvvi.insn[0][0], disass);
+      // Check for compressed instruction and zero out top bits if so
+      compressedInstruction = rvvi.insn[0][0][1:0] != 2'b11;
+      trimmedInsn = compressedInstruction ? {16'b0, rvvi.insn[0][0][15:0]} : rvvi.insn[0][0];
+      // Generate disassembly string
+      disass = disassemble(trimmedInsn);
+      // When sampling, format instruction as 16 or 32 bits depending on if it was compressed
+      if (compressedInstruction) begin
+        riscvISACOV.sample(rvvi.trap[0][0], 0, 0, {$sformatf("%04h ", trimmedInsn), disass});
+        // $display("cvw_arch_verif: sample taken for instruction %04h: %s", trimmedInsn, disass);
+        $display("0x%04h: %s", trimmedInsn, disass);
+      end else begin
+        riscvISACOV.sample(rvvi.trap[0][0], 0, 0, {$sformatf("%08h ", trimmedInsn), disass});
+        // $display("cvw_arch_verif: sample taken for instruction %08h: %s", trimmedInsn, disass);
+        $display("0x%08h: %s", trimmedInsn, disass);
+      end
     end
   end
 
