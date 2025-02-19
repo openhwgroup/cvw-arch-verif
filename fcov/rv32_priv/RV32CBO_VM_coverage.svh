c30ee35a71db5d17b2906c68faa5a2124839df4f
Jordan Carlin <jordanmcarlin@gmail.com>
2025-02-19 01:20:55 -0800

    Update RVVI interface to integrate VM signals

--- fcov/rv32_priv/RV32CBO_VM_coverage.svh
+++ fcov/rv32_priv/RV32CBO_VM_coverage.svh
@@ -23,62 +23,62 @@ typedef RISCV_instruction #(ILEN, XLEN, FLEN, VLEN, NHART, RETIRE) ins_rv32cbo_v
 covergroup RV32CBO_VM_exceptions_cg with function sample(ins_rv32cbo_vm_t ins);
     option.per_instance = 0; 
     //pte permission for leaf PTEs
-    PTE_d_inv: coverpoint ins.current.PTE_d[7:0] { //exp.1
+    PTE_d_inv: coverpoint ins.current.pte_d[7:0] { //exp.1
         wildcard bins leaflvl_u_w = {8'b???1?110};
         wildcard bins leaflvl_s_w = {8'b???0?110};
     }
 
-    PTE_d_res_rwx: coverpoint ins.current.PTE_d[7:0] { //exp.2
+    PTE_d_res_rwx: coverpoint ins.current.pte_d[7:0] { //exp.2
         wildcard bins leaflvl_exec_u = {8'b???11101};
         wildcard bins leaflvl_noexec_u = {8'b???10101};
         wildcard bins leaflvl_exec_s = {8'b???01101};
         wildcard bins leaflvl_noexec_s = {8'b???00101};
     }
 
-    PTE_nonleaf_lvl0_d: coverpoint ins.current.PTE_d[7:0] { //exp.3
+    PTE_nonleaf_lvl0_d: coverpoint ins.current.pte_d[7:0] { //exp.3
         wildcard bins lvl0_s = {8'b???00001};
         wildcard bins lvl0_u = {8'b???10001};
     }
 
-    PTE_rw_spage_d: coverpoint ins.current.PTE_d[7:0] { //exp.4 & 5
+    PTE_rw_spage_d: coverpoint ins.current.pte_d[7:0] { //exp.4 & 5
         wildcard bins leaflvl_w_0 = {8'b???0?0?1};
     }
 
-    PTE_spage_d: coverpoint ins.current.PTE_d[7:0] { //exp.6
+    PTE_spage_d: coverpoint ins.current.pte_d[7:0] { //exp.6
         wildcard bins leaflvl_s = {8'b???01111};
     }
 
-    PTE_upage_d: coverpoint ins.current.PTE_d[7:0] { //exp.7
+    PTE_upage_d: coverpoint ins.current.pte_d[7:0] { //exp.7
         wildcard bins leaflvl_u = {8'b???11111};
     }
 
-    PTE_rw_upage_d: coverpoint ins.current.PTE_d[7:0] { //exp.8
+    PTE_rw_upage_d: coverpoint ins.current.pte_d[7:0] { //exp.8
         wildcard bins leaflvl_w_0 = {8'b???1?0?1};
     }
 
-    PTE_Abit_unset_d: coverpoint ins.current.PTE_d[7:0] { //exp.9
+    PTE_Abit_unset_d: coverpoint ins.current.pte_d[7:0] { //exp.9
         wildcard bins leaflvl_u = {8'b?0?11111};
         wildcard bins leaflvl_s = {8'b?0?01111};
     }
 
-    PTE_Dbit_set_W_d: coverpoint ins.current.PTE_d[7:0] { //exp.10
+    PTE_Dbit_set_W_d: coverpoint ins.current.pte_d[7:0] { //exp.10
         wildcard bins leaflvl_u = {8'b01?1?111};
         wildcard bins leaflvl_s = {8'b01?0?111};
     }
 
-    PTE_RWX_d: coverpoint ins.current.PTE_d[7:0] { //exp.11
+    PTE_RWX_d: coverpoint ins.current.pte_d[7:0] { //exp.11
         wildcard bins leaflvl_u = {8'b???11111};
         wildcard bins leaflvl_s = {8'b???01111};
     }
 
     //PageType && misaligned PPN for DTLB to ensure that leaf pte is found at all levels (through crosses of PTE and PPN)
 
-    PageType_d: coverpoint ins.current.PageType_d {
+    PageType_d: coverpoint ins.current.page_type_d {
         bins mega = {2'b01};
         bins kilo = {2'd0};
     }
 
-    misaligned_PPN_d: coverpoint ins.current.PPN_d[9:0] {
+    misaligned_PPN_d: coverpoint ins.current.ppn_d[9:0] {
         bins mega_not_zero = {[1:$]};
     }
 
@@ -88,7 +88,7 @@ covergroup RV32CBO_VM_exceptions_cg with function sample(ins_rv32cbo_vm_t ins);
     }
 
     //For crosses with write accesses and its corresponding faults
-    write_acc: coverpoint ins.current.WriteAccess{
+    write_acc: coverpoint ins.current.write_access{
         bins set = {1};
     }
     Mcause: coverpoint  ins.current.csr[12'h342] iff (ins.trap == 1) {
@@ -175,7 +175,6 @@ function void rv32cbo_vm_sample(int hart, int issue);
 
     ins = new(hart, issue, traceDataQ); 
     ins.add_csr(0);
-    ins.add_vm_signals(1);
     
     RV32CBO_VM_exceptions_cg.sample(ins);
 endfunction
