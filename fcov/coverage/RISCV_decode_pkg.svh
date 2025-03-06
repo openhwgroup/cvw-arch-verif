95e5a066f676f8aab4c7feaf5093ec6dbea2e276
jacassidy <jkc.cassidy@gmail.com>
2025-03-06 08:51:42 -0800

    Added copyright

--- fcov/coverage/RISCV_decode_pkg.svh
+++ fcov/coverage/RISCV_decode_pkg.svh
@@ -1,4 +1,23 @@
-
+///////////////////////////////////////////
+//
+// RISC-V Architectural Functional Coverage Instruction Disassembler Helper Functions
+//
+// Copyright (C) 2025 Harvey Mudd College, 10x Engineers, UET Lahore, Habib University
+// Written: Kaden Cassidy jacassidy@hmc.edu March 2025
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file 
+// except in compliance with the License, or, at your option, the Apache License version 2.0. You 
+// may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.1/
+//
+// Unless required by applicable law or agreed to in writing, any work distributed under the 
+// License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+// either express or implied. See the License for the specific language governing permissions 
+// and limitations under the License.
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 package RISCV_decode_pkg;
 
