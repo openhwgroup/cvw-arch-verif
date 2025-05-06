fcfad119b5a08b5ed7aecad5d357cf315a98d4ca
Mysterio-Abdullah <mrabdullahmib381@gmail.com>
2025-05-06 13:12:49 -0700

    walking ones cp

--- fcov/coverage/RISCV_coverage_config.svh
+++ fcov/coverage/RISCV_coverage_config.svh
@@ -146,6 +146,12 @@
 `ifdef RV32VM_COVERAGE
   `include "RV32VM_coverage.svh"
 `endif
+`ifdef RV64VM_PMP_COVERAGE
+  `include "RV64VM_PMP_coverage.svh"
+`endif
+`ifdef RV32VM_PMP_COVERAGE
+  `include "RV32VM_PMP_coverage.svh"
+`endif
 `ifdef RV64PMP_COVERAGE
   `include "PMPM_coverage.svh"
 `endif
