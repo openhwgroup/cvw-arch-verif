4e2a50b74b5caa1c7752c1e856e33416960f7107
Noah Fotenos <nfotenos@g.hmc.edu>
2025-05-28 11:16:37 -0700

    added floating signitures for hazard tests

--- bin/testgen.py
+++ bin/testgen.py
@@ -1061,8 +1061,14 @@ def writeHazardVector(desc, rs1a, rs2a, rda, rs1b, rs2b, rdb, testb, immvala, im
                     ["perform first operation", "perform second (triggering) operation"],
                     xlen)
     #lines += f"addi x{rs2b}, x{rs2b}, 4 # should be skipped\n"
-    lines += writeSIGUPD(rda)
-    lines += writeSIGUPD(rdb)
+    if testa in floattypes and testa not in fTOrtype:
+      lines += writeSIGUPD_F(rda)
+    else:
+      lines += writeSIGUPD(rda)
+    if testb in floattypes and testb not in fTOrtype:
+      lines += writeSIGUPD_F(rdb)
+    else:
+      lines += writeSIGUPD(rdb)
     #lines += writeSIGUPD(rs2b)
 
   f.write(lines)
@@ -2560,6 +2566,10 @@ PX2Ftype = ["fmvp.d.x"] # pair of integer registers to a single fp register
 fcomptype = ["feq.s", "flt.s", "fle.s", "fltq.s", "fleq.s",
               "feq.h", "flt.h", "fle.h", "fltq.h", "fleq.h",
               "feq.d", "flt.d", "fle.d", "fltq.d", "fleq.d",]
+fTOrtype  = ["feq.s", "feq.h", "feq.d", "flt.s", "flt.h", "flt.d", "fle.s", "fle.h", "fle.d",
+             "fltq.s", "fltq.h", "fltq.d", "fleq.s", "fleq.h", "fleq.d", "fclass.s", "fclass.h", "fclass.d"
+              "fcvt.dst.src", "fltq.s", "fltq.h", "fltq.d", "fleq.s", "fleq.h", "fleq.d",
+              "fmvp.x.q", "fcvtmod.w.d"] # *All* floating point instructions that return to xregisters (rd) *(I think all (fcvt.dist.src may be exeption))
 citype = ["c.nop", "c.lui", "c.li", "c.addi", "c.addi16sp", "c.addiw","c.lwsp","c.ldsp","c.flwsp","c.fldsp"]
 c_shiftitype = ["c.slli","c.srli","c.srai"]
 cltype = ["c.lw","c.ld","c.flw","c.fld"]
