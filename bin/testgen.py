0778565e233e979dbdac999efc94685439d32974
David Harris <David_Harris@hmc.edu>
2025-06-04 11:09:37 -0700

    Whitespace

--- bin/testgen.py
+++ bin/testgen.py
@@ -2400,7 +2400,7 @@ def write_tests(coverpoints, test, xlen=None, vlen=None, sew=None, vlmax=None, v
       make_sbox(test, xlen)
     elif (coverpoint in ["cp_sc", "cp_prev_lr", "cp_prev_sc", "cp_custom_sc_after_sc", "cp_custom_sc_after_store",
                          "cp_custom_sc_after_load", "cp_sc_fail", "cp_address_difference", "cp_custom_sc_lrsc",
-                         "cp_custom_sc_addresses", "cp_custom_rd_corners", "cp_custom_svinval"]):
+                         "cp_custom_sc_addresses", "cp_custom_rd_corners"]):
       pass # Zalrsc coverpoints handled custom
     elif (coverpoint in ["cp_custom_aqrl", "cp_custom_fencei"]):
       make_custom(test, xlen)
