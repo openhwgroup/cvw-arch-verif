a6d4e6add1a03d06f7b0a7b6a5bad9d0352d6f12
David Harris <David_Harris@hmc.edu>
2025-06-09 08:08:49 -0700

    Fixed eof

--- fcov/priv/Svinval_coverage.svh
+++ fcov/priv/Svinval_coverage.svh
@@ -19,66 +19,29 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 `define COVER_SVINVAL
-covergroup Svinval_sfence_inval_ir_cg with function sample(ins_t ins);
+covergroup Svinval_cg with function sample(ins_t ins);
     option.per_instance = 0;
-    cp_asm_count : coverpoint ins.ins_str == "sfence.inval.ir" {
-        bins goal = {1};
+    cp_instr : coverpoint ins.current.insn {
+        wildcard bins sfence_inval_ir = {32'b0001100_00001_00000_000_00000_1110011};
+        wildcard bins sfence_w_inval  = {32'b0001100_00000_00000_000_00000_1110011};
+        wildcard bins sinval_vma      = {32'b0001011_?????_?????_000_00000_1110011};
+        wildcard bins sfence_vma      = {32'b0001001_?????_?????_000_00000_1110011}; // not essential, but might as well cross it
     }
-    priv_tvm : coverpoint {ins.prev.mode, ins.prev.csr[12'h300][20]} {
-        bins M_mode     = {3'b11_0};
-        bins S_mode     = {3'b01_0};
-        bins S_mode_tvm = {3'b01_1};
-        bins U_mode     = {3'b00_0};
+    cp_priv : coverpoint {ins.prev.mode} {
+        bins M_mode     = {2'b11};
+        bins S_mode     = {2'b01};
+        bins U_mode     = {2'b00};
     }
-    cr_instr_mode : cross cp_asm_count, priv_tvm {
-        // instruction executed in each mode
+    cp_tvm : coverpoint ins.prev.csr[12'h300][20] {
+    }
+    cr_svinival : cross cp_instr, cp_priv, cp_tvm {
+        // each instruction executed in each privilege mode with each TVM
     }
  endgroup
-// ---------------------
-covergroup Svinval_sfence_w_inval_cg with function sample(ins_t ins);
-    option.per_instance = 0;
-    cp_asm_count : coverpoint ins.ins_str == "sfence.w.inval"  {
-        bins goal = {1};
-    }
-    priv_tvm : coverpoint {ins.prev.mode, ins.prev.csr[12'h300][20]} {
-        bins M_mode     = {3'b11_0};
-        bins S_mode     = {3'b01_0};
-        bins S_mode_tvm = {3'b01_1};
-        bins U_mode     = {3'b00_0};
-    }
-    cr_instr_mode : cross cp_asm_count, priv_tvm {
-        // instruction executed in each mode
-    }
-endgroup
-// ---------------------
-covergroup Svinval_sinval_vma_cg with function sample(ins_t ins);
-    option.per_instance = 0;
-    cp_asm_count : coverpoint ins.ins_str == "sinval.vma"   {
-        bins goal = {1};
-    }
-    priv_tvm : coverpoint {ins.prev.mode, ins.prev.csr[12'h300][20]} {
-        bins M_mode     = {3'b11_0};
-        bins S_mode     = {3'b01_0};
-        bins S_mode_tvm = {3'b01_1};
-        bins U_mode     = {3'b00_0};
-    }
-    cr_instr_mode : cross cp_asm_count, priv_tvm {
-        // instruction executed in each mode
-    }
-endgroup
+
 // ---------------------
 function void svinval_sample(int hart, int issue, ins_t ins);
 
     //$display("Svinval coverage: ins_str %s ins,prev.mode %b tvm %b", ins.ins_str, ins.prev.mode, ins.prev.csr[12'h300][20]);
-    case (traceDataQ[hart][issue][0].inst_name)
-        "sfence.inval.ir"     : begin
-            Svinval_sfence_inval_ir_cg.sample(ins);
-        end
-        "sfence.w.inval"     : begin
-            Svinval_sfence_w_inval_cg.sample(ins);
-        end
-        "sinval.vma"     : begin
-            Svinval_sinval_vma_cg.sample(ins);
-        end
-    endcase
+    Svinval_cg.sample(ins);
 endfunction
--- fcov/priv/Svinval_coverage_init.svh
+++ fcov/priv/Svinval_coverage_init.svh
@@ -7,6 +7,4 @@
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
-    Svinval_sfence_inval_ir_cg = new(); Svinval_sfence_inval_ir_cg.set_inst_name("obj_Svinval_sfence_inval_ir");
-    Svinval_sfence_w_inval_cg = new(); Svinval_sfence_w_inval_cg.set_inst_name("obj_Svinval_sfence_w_inval");
-    Svinval_sinval_vma_cg = new(); Svinval_sinval_vma_cg.set_inst_name("obj_Svinval_sinval_vma");
+    Svinval_cg = new(); Svinval_cg.set_inst_name("obj_Svinval");
