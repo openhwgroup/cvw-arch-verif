669eefdbb86bd64659b8eb8977e596d608b37a48
Noah Fotenos <nfotenos@g.hmc.edu>
2025-07-10 16:09:21 -0700

    cleanup

--- fcov/priv/ZicsrU_coverage.svh
+++ fcov/priv/ZicsrU_coverage.svh
@@ -30,11 +30,6 @@ covergroup ZicsrU_uprivinst_cg with function sample(ins_t ins);
     privinstrs: coverpoint ins.current.insn  {
         bins ecall  = {32'h00000073};
         bins ebreak = {32'h00100073};
-        // fences are not really privileged instructions, but are tested here for lack of a more convenient place
-       // bins fence =  {32'h0ff0000f}; // fence iorw, iorw
-        //bins fence_rw_rw = {32'h0330000f}; // fence rw, rw
-        //bins fence_tso_rw_rw = {32'h8330000f}; // fence.tso
-        //bins pause = {32'h0100000F}; // pause, for Zihintpause
     }
     mret: coverpoint ins.current.insn  {
         bins mret   = {32'h30200073};
