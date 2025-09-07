c42a8fd1e3bc4ca28030637e896790dac9d45f17
Jordan Carlin <jordanmcarlin@gmail.com>
2025-09-07 11:34:30 -0700

    Update makefile_gen for multistage compilation

--- framework/makefile_gen.py
+++ framework/makefile_gen.py
@@ -14,28 +14,57 @@ from typing import Any
 
 def generate_makefile(test_list: dict[str, dict[str, Any]], makefile_path: Path, wkdir: Path) -> None:
     """Generate a Makefile to run the selected tests."""
+    xlen = 64  # TODO: Get from config
+    mabi = f"{'i' if xlen == 32 else ''}lp{xlen}"
+    include_paths = "-Itests -Itests/env -Itests/priv/headers"
+    linker_script = "tests/link.ld"
     with makefile_path.open("w") as makefile:
         # General variables
-        makefile.write("CC := riscv64-unknown-elf-gcc\n")
         makefile.write(
-            "CFLAGS := -O0 -g -mcmodel=medany -nostartfiles -T tests/link.ld -Itests -Itests/env -Itests/priv/headers\n"
+            f"XLEN := {xlen}\n"
+            "CC := riscv64-unknown-elf-gcc\n"
+            f"INCLUDE_PATHS := {include_paths}\n"
+            f"CFLAGS := -O0 -g -mcmodel=medany -nostartfiles -T {linker_script} ${{INCLUDE_PATHS}}\n"
         )
         # Top-level target to compile all tests
         makefile.write("TESTS = \\\n")
         for test_name in test_list.keys():
-            makefile.write(f"    {test_name.replace('.S', '.elf')} \\\n")
+            makefile.write(f"    {wkdir}/{test_name.replace('.S', '.elf')} \\\n")
         makefile.write("\nall: $(TESTS)\n\n")
         # Individual test compilation targets
         for test_name, test_metadata in test_list.items():
-            elf = wkdir / test_name.replace(".S", ".elf")
-            makefile.write(f"{elf}: tests/{test_name} | {elf.parent}\n")
-            makefile.write(f"\t@echo Compiling {test_name} to {elf}\n")
+            march = test_metadata.get("MARCH")
+            flen = "64" if "D" in test_metadata.get("implemented_extensions") else "32"
+            # Generate signature based ELF
+            sig_elf = wkdir / test_name.replace(".S", "-sig.elf")
+            makefile.write(f"{sig_elf}: tests/{test_name} | {sig_elf.parent}\n")
+            makefile.write(f"\t@echo Compiling {test_name} to {sig_elf}\n")
             makefile.write(
-                f"\t$(CC) $(CFLAGS) -o {elf} -march={test_metadata.get('MARCH')} -mabi=lp64 -DLOCKSTEP -DXLEN=64 tests/{test_name}\n\n"
+                f"\t$(CC) $(CFLAGS) -o {sig_elf} -march={march} -mabi={mabi} -DSIGNATURE -DXLEN={xlen} -DFLEN={flen} tests/{test_name}\n\n"
             )
+
+            # Generate signature file
+            sig_file = wkdir / test_name.replace(".S", ".sig")
+            sig_log_file = wkdir / test_name.replace(".S", ".log")
+            makefile.write(f"{sig_file}: {sig_elf}\n")
+            makefile.write(f"\t@echo Generating signature for {sig_elf} to {sig_file}\n")
+            makefile.write(f"\tsail_riscv_sim --test-signature={sig_file} --signature-granularity {int(xlen/8)} {sig_elf} > {sig_log_file}\n\n")
+
+            # Final ELF target
+            final_elf = wkdir / test_name.replace(".S", ".elf")
+            makefile.write(f"{final_elf}: {sig_elf} {sig_file}\n")
+            makefile.write(f"\t@echo Generating final ELF {final_elf}\n")
+            makefile.write(
+                f"\t$(CC) $(CFLAGS) -o {final_elf} -march={march} -mabi={mabi} -DSELFCHECK -DXLEN={xlen} -DFLEN={flen} -DSIGNATURE_FILE='{sig_file}' tests/{test_name}\n\n"
+            )
+
         # Directory creation rules
         makefile.write(f"{wkdir}/%:\n")
         makefile.write("\tmkdir -p $@\n\n")
+
+        # Clean target
+        makefile.write("clean:\n")
+        makefile.write(f"\trm -rf {wkdir}/*\n")
 
 
 def main():
