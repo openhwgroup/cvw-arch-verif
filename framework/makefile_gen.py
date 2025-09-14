84f9f578fc7b147e2fe578637dadc71757722116
Jordan Carlin <jordanmcarlin@gmail.com>
2025-09-14 16:25:27 -0700

    Don't allow individual scripts to be executed

--- framework/makefile_gen.py
+++ framework/makefile_gen.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 ##################################
 # makefile_gen.py
 #
--- framework/parse_udb_config.py
+++ framework/parse_udb_config.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 ##################################
 # parse_udb_config.py
 #
