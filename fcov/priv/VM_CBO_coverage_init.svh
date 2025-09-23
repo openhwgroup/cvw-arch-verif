9f26fc468e8b33edca6497c77ecc2d8b68c8ea05
Zain2050 <zainzahid2050@gmail.com>
2025-09-23 04:51:22 -0700

    Added a combined XLEN coverage file for VM_CBO

---
+++ fcov/priv/VM_CBO_coverage_init.svh
@@ -0,0 +1,11 @@
+///////////////////////////////////////////
+//
+// RISC-V Architectural Functional Coverage Covergroups Initialization File
+//
+// Copyright (C) 2024 Harvey Mudd College, 10x Engineers, UET Lahore, Habib University
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+VM_CBO_exceptions_cg = new(); VM_CBO_exceptions_cg.set_inst_name("obj_VM_CBO_exceptions");
