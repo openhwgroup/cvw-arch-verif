ed1c5ab8702d2d7128e589e6b2846089ee04194a
Jordan Carlin <jordanmcarlin@gmail.com>
2025-10-16 22:36:05 -0700

    Add label for each cp to make debug easier

--- generators/tests/testgen/testgen.py
+++ generators/tests/testgen/testgen.py
@@ -132,6 +132,7 @@ def generate_tests_for_instruction(
             continue
         # Get the coverpoint specific handler and generate tests
         else:
+            test_lines.append(f"{coverpoint}_tests:")
             test_lines.append(generate_tests_for_coverpoint(instr_name, instr_type, coverpoint, test_data))
 
     # Combine test lines into a single string
